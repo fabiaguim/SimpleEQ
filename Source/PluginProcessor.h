049977db1a4e9be36f1c1110696579a277440db2
fabiaguim <167236833+fabiaguim@users.noreply.github.com>
2025-11-22 14:42:10 +0100

    Setting up the DSP

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -62,6 +62,17 @@ public:
 
 
 private:
+
+    // Prepare filters in PrepareToPlay before using them
+
+    using Filter = juce::dsp::IIR::Filter<float>;
+
+    using CutFilter = juce::dsp::ProcessorChain<Filter, Filter, Filter, Filter>;
+
+    using MonoChain = juce::dsp::ProcessorChain<CutFilter, Filter, CutFilter>;
+
+    MonoChain leftChain, rightChain;
+
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessor)
 };
